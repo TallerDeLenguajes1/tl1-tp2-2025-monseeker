8fcf60677401a982a91fe788105e01b4977f05e7
monseeker <guesam460@gmail.com>
2025-04-01 12:34:46 -0900

    Genera numeros usando aritemetica de punteros

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -8,13 +8,10 @@ int main(){
     int i;
     double vt[N];
     for(i = 0;i<N; i++){
-        vt[i]=1+rand()%100;
-
-        //recorro el arreglo con notacion subindexada
-        printf("%f  ", vt[i]);
+        *(vt + i)=1+rand()%100;
 
         //recorro el arreglo con notacion indexada
-        printf("%f  ", *(vt+i));
+        printf("%f \n", *(vt+i));
     }
 
     return 0;
